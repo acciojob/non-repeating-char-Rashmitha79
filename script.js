7893389a271cc03b273975b72c98836c3ae70d21
Rashmitha79 <134279042+Rashmitha79@users.noreply.github.com>
2023-09-11 21:24:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  if (str.length === 0) {
+    return null; // Return null for empty strings
+  }
 
-    if (!str) {
-        return null;
-    }
+  const charCount = {}; // Object to store character frequencies
 
-    const charCount = new Array(26).fill(0); // Assuming only lowercase English letters
-    
-    // Count the occurrences of each character in the string
-    for (let i = 0; i < str.length; i++) {
-        const char = str[i];
-        charCount[char.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+  // Count the frequency of each character in the string
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+
+  // Find the first character with a frequency of 1
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (charCount[char] === 1) {
+      return char; // Return the first non-repeated character
     }
-    
-    // Find the first character with a count of 1
-    for (let i = 0; i < str.length; i++) {
-        const char = str[i];
-        if (charCount[char.charCodeAt(0) - 'a'.charCodeAt(0)] === 1) {
-            return char;
-        }
-    }
+  }
+
+  return null; // If all characters are repeated
+}
+
+// Test cases
+console.log(firstNonRepeatedChar('aabbcdd')); // Output: 'c'
+console.log(firstNonRepeatedChar('aabbcc'));  // Output: null
